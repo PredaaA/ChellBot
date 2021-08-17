7b280f9caf602c6d40bc9b52f49d9b65bda3897c
PredaaA <46051820+PredaaA@users.noreply.github.com>
2021-08-17 19:54:11 +0200

    handle input on help cmd.

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,4 +1,5 @@
 import { Message } from "revolt.js/dist/maps/Messages";
+import { Command } from "../types/command";
 
 module.exports = {
     name: 'help',
@@ -7,10 +8,25 @@ module.exports = {
     developer: false,
 
     run: async (msg: Message, args: string[]) => {
+        const input = args.join(' ');
         let content = `# ${msg.client.user?.username} help\n---\n`;
-        // @ts-ignore
-        for (const cmd of msg.client.framework.commands) {
-            content += `- **${cmd.name}:** ${cmd.description || 'No description.'}\n`
+        if (!input) {
+            // @ts-ignore
+            for (const cmd of msg.client.framework.commands) {
+                content += `- **${cmd.name}:** ${cmd.description || 'No description.'}\n`
+            }
+        } else {
+            // @ts-ignore
+            const cmd: Command = msg.client.framework.getCommand(input)
+            if (!cmd) {
+                content += 'Nothing found!'
+            } else {
+                content += (
+                    `**${cmd.name}**\n${cmd.description || 'No description.'}\n`
+                    + `**Usage:** \`${msg.content.substring(0, 1) + cmd.name}\`\n`
+                    + `**Aliases:** ${cmd.aliases.join(', ')}\n`
+                )
+            }
         }
         msg.channel?.sendMessage(content);
     }
